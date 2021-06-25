592893a3a277b2359d63ad5de8bf23b802e5f1e1
Ayesha Khan <nawal.khan@northsouth.edu>
2021-06-25 13:22:04 +0600

    Follow funtions added and buttons added

--- FreeCodeGram/app/Http/Controllers/ProfilesController.php
+++ FreeCodeGram/app/Http/Controllers/ProfilesController.php
@@ -10,8 +10,9 @@ class ProfilesController extends Controller
 {
    public function index(User $user)
     {
+		$follows=(auth()->user()) ? auth()->user()->following->contains($user->id):false;
 		
-        return view('profiles.index',compact('user'));
+        return view('profiles.index',compact('user','follows'));
     } 
 	
 	public function edit(User $user)
@@ -39,11 +40,12 @@ class ProfilesController extends Controller
 			$imagePath=request('image')->store('profile','public');
 			$image=Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
 			$image->save();
+			$imageArray=['image'=>$imagePath];
 		}
 		
 		auth()->user()->profile->update(array_merge(
 			$data,
-			['image'=>$imagePath ]
+			$imageArray ?? []
 		));
 		
 		return redirect("/profile/{$user->id}");
